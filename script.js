58e52ddca599cc9abff57df147c2f69b4c9a946c
Bojo <bozhidar_zahariev@hotmail.com>
2019-05-10 17:28:00 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+var bars = document.getElementById("bars")
+var nav = document.getElementById("myTopnav");
 
+
+function miniNav() {
+    if (nav.className === "navigation") {
+        nav.className += " responsive";
+    } else {
+        nav.className = "navigation";
+    }
+}
+
+bars.addEventListener('click', () => {
+    miniNav();
+    /*setTimeout(
+        function () {
+            nav.className = "navigation";
+        }, 7000);*/
+}, false);
