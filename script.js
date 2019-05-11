0caa9c38cbbf66ce95172c6983d554aa40f66ffa
Bojo <bozhidar_zahariev@hotmail.com>
2019-05-11 14:47:57 +0100

    working slideshow

--- script.js
+++ script.js
@@ -22,3 +22,51 @@ bars.addEventListener('click', () => {
 arrow.addEventListener('click', () => {
 	miniNav();
 }, false);
+
+
+//Slides
+
+var slideLeft = document.getElementById("slideLeft");
+var slideRight = document.getElementById("slideRight");
+
+slideLeft.addEventListener('click', () => {
+	currentSlide(1);
+}, false);
+
+slideRight.addEventListener('click', () => {
+	currentSlide(2);
+}, false);
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+	showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+	showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("mySlides");
+	var dots = document.getElementsByClassName("demo");
+	var captionText = document.getElementById("caption");
+	if (n > slides.length) {
+		slideIndex = 1
+	}
+	if (n < 1) {
+		slideIndex = slides.length
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+	captionText.innerHTML = dots[slideIndex - 1].alt;
+}
