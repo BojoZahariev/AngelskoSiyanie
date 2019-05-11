ae76073e8fb79050d6616351d78e114c29a95361
Bojo <bozhidar_zahariev@hotmail.com>
2019-05-11 10:53:01 +0100

    improved top

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-var bars = document.getElementById("bars")
-var nav = document.getElementById("myTopnav");
-
+var bars = document.getElementById('bars');
+var nav = document.getElementById('myTopnav');
+var arrow = document.getElementById('arrow');
 
 function miniNav() {
-    if (nav.className === "navigation") {
-        nav.className += " responsive";
-    } else {
-        nav.className = "navigation";
-    }
+	if (nav.className === 'navigation') {
+		nav.className += ' responsive';
+		bars.style.display = 'none';
+		arrow.style.display = 'block';
+	} else {
+		nav.className = 'navigation';
+		bars.style.display = 'block';
+		arrow.style.display = 'none';
+	}
 }
 
 bars.addEventListener('click', () => {
-    miniNav();
-    /*setTimeout(
-        function () {
-            nav.className = "navigation";
-        }, 7000);*/
+	miniNav();
+}, false);
+
+
+arrow.addEventListener('click', () => {
+	miniNav();
 }, false);
