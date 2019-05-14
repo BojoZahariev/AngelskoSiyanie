f78b6b9b4e88c739cabf358e6a4449a47860e5f6
Bojo <bozhidar_zahariev@hotmail.com>
2019-05-14 12:28:28 +0100

    different slide style

--- script.js
+++ script.js
@@ -26,6 +26,8 @@ arrow.addEventListener('click', () => {
 
 //Slides
 
+/*
+
 var slideLeft = document.getElementById("slideLeft");
 var slideRight = document.getElementById("slideRight");
 
@@ -70,6 +72,42 @@ function showSlides(n) {
 	dots[slideIndex - 1].className += " active";
 	captionText.innerHTML = dots[slideIndex - 1].alt;
 }
+*/
+
+
+var slideIndex = [1, 1, 1];
+var slideId = ["mySlides1", "mySlides2", "mySlides3"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+
+
+function plusSlides(n, no) {
+	showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+	var i;
+	var x = document.getElementsByClassName(slideId[no]);
+	if (n > x.length) {
+		slideIndex[no] = 1
+	}
+	if (n < 1) {
+		slideIndex[no] = x.length
+	}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[slideIndex[no] - 1].style.display = "block";
+}
+
+
+
+
+
+
+
+
 
 
 //logo shrink 
